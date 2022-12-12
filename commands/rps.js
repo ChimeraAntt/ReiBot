c56978dd7eb52eb50691b3945897cc7bcb701448
Chimera <lucyzs1010@gmail.com>
2022-12-12 17:03:07 -0500

    Added Rock Paper Scizzors

---
+++ commands/rps.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder, CommandInteractionOptionResolver } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("rps")
+        .setDescription("Rock Paper Scizzors")
+        .addStringOption((option) => option
+			.setName('rps')
+			.setDescription('Choose')
+            .setRequired(true)
+			.addChoices({ name: 'Rock', value: 'rock' }, { name: 'Paper', value: 'paper' }, { name: 'Scizzors', value: 'scizzors'})),    
+    async execute(interaction){
+        const userOption = interaction.options.getString("rps");
+        const botOptions = ["rock", "paper", "scizzors"];
+        const botChoice = botOptions[Math.floor(Math.random()*botOptions.length)];
+        console.log(botChoice);
+        console.log(userOption);
+
+
+
+        if(userOption == 'rock'){
+            if(botChoice == 'rock'){
+                await interaction.reply("Rock ties with Rock");
+            }else if(botChoice == 'paper'){
+                await interaction.reply("Rock loses to Paper L");
+            }else if(botChoice == 'scizzors'){
+                await interaction.reply("Rock beats Scizzors! W");
+            }
+        }else if(userOption == 'paper'){
+            if(botChoice == 'rock'){
+                await interaction.reply("Paper beats Rock! :)");
+            }else if(botChoice == 'paper'){
+                await interaction.reply("Paper ties with Paper");
+            }else if(botChoice == 'scizzors'){
+                await interaction.reply("Paper loses to Scizzors :(");
+            }
+        }else if(userOption == 'scizzors'){
+            if(botChoice == 'rock'){
+                await interaction.reply("Scizzors loses to Rock...")
+            }else if(botChoice == 'paper'){
+                await interaction.reply("Scizzors Beats Paper!!!")
+            }else if(botChoice == 'scizzors'){
+                await interaction.reply("Scizzors ties with Scizzors???")
+            }
+        }else{
+            await interaction.reply({content: "This is not an option",ephemeral: true});
+        }
+
+    },
+};
