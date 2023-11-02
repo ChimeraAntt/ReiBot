cde080f72b13bf23df6d0b2625855f3b8ad73a74
ChimeraAntt <lucyzs1010@gmail.com>
2023-11-02 13:15:15 -0400

    commit

--- post_request.js
+++
@@ -1,8 +0,0 @@
-fetch("https://id.twitch.tv/oauth2/token", {
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: 'client_id=tmovhfkqxom9z7xkkx46b9jjzn0s9u&client_secret=almavrosf3t119qu5ezcvetlczl69k&grant_type=client_credentials'
-}).then(response => response.json())
-    .then(response => console.log(JSON.stringify(response)))
